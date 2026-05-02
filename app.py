424f5495b14a7006a74358480072001293eb54a5
hiralrana24 <hiral.rana0910@gmail.com>
2026-05-02 10:42:51 +0000

    Ajout histogramme Paris

--- app.py
+++ app.py
@@ -34,6 +34,9 @@ def api_paris():
 def mongraphique():
     return render_template("graphique.html")
 
+@app.route("/histogramme")
+def monhistogramme():
+    return render_template("histogramme.html")
 
 
 # Ne rien mettre après ce commentaire
